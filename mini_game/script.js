545fb514d5c2c639ca144ad01f8b9b10026a936f
MikeWencel <wencel.michael@gmail.com>
2020-05-13 23:19:35 +0200

    Working with input

--- mini_game/script.js
+++ mini_game/script.js
@@ -5,5 +5,20 @@ document.querySelector("button").addEventListener("click", startGame)
 function startGame(){
     
     console.log("gamestart")
+    document.getElementById("begin-quest").innerHTML="What's your name?"
+    addElement();
+    
 }
 
+
+function addElement()
+{
+  // tworzy nowy element div 
+  // i daje jego zawartość 
+  newDiv = document.createElement("div");
+  newDiv.innerHTML = "<input></input>";
+
+  // add the newly created element and it's content into the DOM
+  my_div = document.getElementById("org_div1");
+  document.body.insertBefore(newDiv, my_div);
+}
